5c29bbc811643af0bccb9397846a73b631a4b598
BuildTools <unconfigured@null.spigotmc.org>
2022-06-25 15:05:28 -0600

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿
+namespace WizTecBasicSalesCalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.Apple = new System.Windows.Forms.Button();
+            this.Orange = new System.Windows.Forms.Button();
+            this.Banana = new System.Windows.Forms.Button();
+            this.WindshieldFluid = new System.Windows.Forms.Button();
+            this.Lube = new System.Windows.Forms.Button();
+            this.Pump = new System.Windows.Forms.Button();
+            this.Keyboard = new System.Windows.Forms.Button();
+            this.Mouse = new System.Windows.Forms.Button();
+            this.Mousepad = new System.Windows.Forms.Button();
+            this.LipBalm = new System.Windows.Forms.Button();
+            this.Ointment = new System.Windows.Forms.Button();
+            this.Lotion = new System.Windows.Forms.Button();
+            this.Void = new System.Windows.Forms.Button();
+            this.Export = new System.Windows.Forms.Button();
+            this.Transaction = new System.Windows.Forms.RichTextBox();
+            this.Sales = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // Apple
+            // 
+            this.Apple.Location = new System.Drawing.Point(12, 12);
+            this.Apple.Name = "Apple";
+            this.Apple.Size = new System.Drawing.Size(106, 41);
+            this.Apple.TabIndex = 0;
+            this.Apple.Text = "Apple";
+            this.Apple.UseVisualStyleBackColor = true;
+            this.Apple.Click += new System.EventHandler(this.Apple_Click);
+            // 
+            // Orange
+            // 
+            this.Orange.Location = new System.Drawing.Point(124, 12);
+            this.Orange.Name = "Orange";
+            this.Orange.Size = new System.Drawing.Size(106, 41);
+            this.Orange.TabIndex = 1;
+            this.Orange.Text = "Orange";
+            this.Orange.UseVisualStyleBackColor = true;
+            this.Orange.Click += new System.EventHandler(this.Orange_Click);
+            // 
+            // Banana
+            // 
+            this.Banana.Location = new System.Drawing.Point(236, 12);
+            this.Banana.Name = "Banana";
+            this.Banana.Size = new System.Drawing.Size(106, 41);
+            this.Banana.TabIndex = 2;
+            this.Banana.Text = "Banana";
+            this.Banana.UseVisualStyleBackColor = true;
+            this.Banana.Click += new System.EventHandler(this.Banana_Click);
+            // 
+            // WindshieldFluid
+            // 
+            this.WindshieldFluid.Location = new System.Drawing.Point(12, 61);
+            this.WindshieldFluid.Name = "WindshieldFluid";
+            this.WindshieldFluid.Size = new System.Drawing.Size(106, 41);
+            this.WindshieldFluid.TabIndex = 5;
+            this.WindshieldFluid.Text = "Windshield Fluid";
+            this.WindshieldFluid.UseVisualStyleBackColor = true;
+            this.WindshieldFluid.Click += new System.EventHandler(this.WindshieldFluid_Click);
+            // 
+            // Lube
+            // 
+            this.Lube.Location = new System.Drawing.Point(124, 61);
+            this.Lube.Name = "Lube";
+            this.Lube.Size = new System.Drawing.Size(106, 41);
+            this.Lube.TabIndex = 4;
+            this.Lube.Text = "Lube";
+            this.Lube.UseVisualStyleBackColor = true;
+            this.Lube.Click += new System.EventHandler(this.Lube_Click);
+            // 
+            // Pump
+            // 
+            this.Pump.Location = new System.Drawing.Point(236, 61);
+            this.Pump.Name = "Pump";
+            this.Pump.Size = new System.Drawing.Size(106, 41);
+            this.Pump.TabIndex = 3;
+            this.Pump.Text = "Pump";
+            this.Pump.UseVisualStyleBackColor = true;
+            this.Pump.Click += new System.EventHandler(this.Pump_Click);
+            // 
+            // Keyboard
+            // 
+            this.Keyboard.Location = new System.Drawing.Point(12, 108);
+            this.Keyboard.Name = "Keyboard";
+            this.Keyboard.Size = new System.Drawing.Size(106, 41);
+            this.Keyboard.TabIndex = 11;
+            this.Keyboard.Text = "Keyboard";
+            this.Keyboard.UseVisualStyleBackColor = true;
+            this.Keyboard.Click += new System.EventHandler(this.Keyboard_Click);
+            // 
+            // Mouse
+            // 
+            this.Mouse.Location = new System.Drawing.Point(124, 108);
+            this.Mouse.Name = "Mouse";
+            this.Mouse.Size = new System.Drawing.Size(106, 41);
+            this.Mouse.TabIndex = 10;
+            this.Mouse.Text = "Mouse";
+            this.Mouse.UseVisualStyleBackColor = true;
+            this.Mouse.Click += new System.EventHandler(this.Mouse_Click);
+            // 
+            // Mousepad
+            // 
+            this.Mousepad.Location = new System.Drawing.Point(236, 108);
+            this.Mousepad.Name = "Mousepad";
+            this.Mousepad.Size = new System.Drawing.Size(106, 41);
+            this.Mousepad.TabIndex = 9;
+            this.Mousepad.Text = "Mousepad";
+            this.Mousepad.UseVisualStyleBackColor = true;
+            this.Mousepad.Click += new System.EventHandler(this.Mousepad_Click);
+            // 
+            // LipBalm
+            // 
+            this.LipBalm.Location = new System.Drawing.Point(12, 155);
+            this.LipBalm.Name = "LipBalm";
+            this.LipBalm.Size = new System.Drawing.Size(106, 41);
+            this.LipBalm.TabIndex = 8;
+            this.LipBalm.Text = "Lip Balm";
+            this.LipBalm.UseVisualStyleBackColor = true;
+            this.LipBalm.Click += new System.EventHandler(this.LipBalm_Click);
+            // 
+            // Ointment
+            // 
+            this.Ointment.Location = new System.Drawing.Point(124, 155);
+            this.Ointment.Name = "Ointment";
+            this.Ointment.Size = new System.Drawing.Size(106, 41);
+            this.Ointment.TabIndex = 7;
+            this.Ointment.Text = "Ointment";
+            this.Ointment.UseVisualStyleBackColor = true;
+            this.Ointment.Click += new System.EventHandler(this.Ointment_Click);
+            // 
+            // Lotion
+            // 
+            this.Lotion.Location = new System.Drawing.Point(236, 155);
+            this.Lotion.Name = "Lotion";
+            this.Lotion.Size = new System.Drawing.Size(106, 41);
+            this.Lotion.TabIndex = 6;
+            this.Lotion.Text = "Lotion";
+            this.Lotion.UseVisualStyleBackColor = true;
+            this.Lotion.Click += new System.EventHandler(this.Lotion_Click);
+            // 
+            // Void
+            // 
+            this.Void.Location = new System.Drawing.Point(463, 386);
+            this.Void.Name = "Void";
+            this.Void.Size = new System.Drawing.Size(106, 41);
+            this.Void.TabIndex = 13;
+            this.Void.Text = "Void";
+            this.Void.UseVisualStyleBackColor = true;
+            this.Void.Click += new System.EventHandler(this.Void_Click);
+            // 
+            // Export
+            // 
+            this.Export.Location = new System.Drawing.Point(630, 386);
+            this.Export.Name = "Export";
+            this.Export.Size = new System.Drawing.Size(106, 41);
+            this.Export.TabIndex = 12;
+            this.Export.Text = "Export";
+            this.Export.UseVisualStyleBackColor = true;
+            this.Export.Click += new System.EventHandler(this.Export_Click);
+            // 
+            // Transaction
+            // 
+            this.Transaction.Location = new System.Drawing.Point(453, 12);
+            this.Transaction.Name = "Transaction";
+            this.Transaction.ReadOnly = true;
+            this.Transaction.Size = new System.Drawing.Size(306, 256);
+            this.Transaction.TabIndex = 15;
+            this.Transaction.Text = "";
+            this.Transaction.TextChanged += new System.EventHandler(this.Transaction_TextChanged);
+            // 
+            // Sales
+            // 
+            this.Sales.Location = new System.Drawing.Point(566, 271);
+            this.Sales.Name = "Sales";
+            this.Sales.ReadOnly = true;
+            this.Sales.Size = new System.Drawing.Size(182, 109);
+            this.Sales.TabIndex = 16;
+            this.Sales.Text = "";
+            this.Sales.TextChanged += new System.EventHandler(this.Sales_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Sales);
+            this.Controls.Add(this.Transaction);
+            this.Controls.Add(this.Void);
+            this.Controls.Add(this.Export);
+            this.Controls.Add(this.Keyboard);
+            this.Controls.Add(this.Mouse);
+            this.Controls.Add(this.Mousepad);
+            this.Controls.Add(this.LipBalm);
+            this.Controls.Add(this.Ointment);
+            this.Controls.Add(this.Lotion);
+            this.Controls.Add(this.WindshieldFluid);
+            this.Controls.Add(this.Lube);
+            this.Controls.Add(this.Pump);
+            this.Controls.Add(this.Banana);
+            this.Controls.Add(this.Orange);
+            this.Controls.Add(this.Apple);
+            this.Name = "Form1";
+            this.Text = "Sales Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button Apple;
+        private System.Windows.Forms.Button Orange;
+        private System.Windows.Forms.Button Banana;
+        private System.Windows.Forms.Button WindshieldFluid;
+        private System.Windows.Forms.Button Lube;
+        private System.Windows.Forms.Button Pump;
+        private System.Windows.Forms.Button Keyboard;
+        private System.Windows.Forms.Button Mouse;
+        private System.Windows.Forms.Button Mousepad;
+        private System.Windows.Forms.Button LipBalm;
+        private System.Windows.Forms.Button Ointment;
+        private System.Windows.Forms.Button Lotion;
+        private System.Windows.Forms.Button Void;
+        private System.Windows.Forms.Button Export;
+        private System.Windows.Forms.RichTextBox Transaction;
+        private System.Windows.Forms.RichTextBox Sales;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//1 - An interface showing products, the transaction,
+//subtotal amount, tax amount, the final amount,
+//a cancel/void button, and a save/export button.
+
+namespace WizTecBasicSalesCalc
+{
+    public partial class Form1 : Form
+    {
+        //Set up base variables and the object class for output
+        //Output will consist for each button and will have:
+        //a price, type if it's taxable, and item type to output on the textbox.
+        List<Product> output = new List<Product>();
+        double VAT = 0.05;
+        double subTotal = 0.0;
+        double Tax = 0.0;
+        double Total = 0.0;
+        List<string> salesText = new List<string>();
+
+        //2- There must be at least 10 different products,
+        //each with their own price, option to apply a 10% VAT tax,
+        //and belonging to 1 of 3 groups.
+        //You are free to decide what the products, groups, and prices are.
+        class Product
+        {
+            public double price { get; set; }
+            public string type { get; set; }
+            public string item { get; set; }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            salesText.Add("Subtotal: ");
+            salesText.Add("Tax: ");
+            salesText.Add("Total: ");
+            changeSalesText();
+
+        }
+
+        //For each of the twelve buttons when pressed:
+        //Adds to the output list with the three variables
+        //Add the selected product to the textbox
+        //Update/calculate the current subtotal, tax, and total
+
+        //Start of product buttons
+        private void Apple_Click(object sender, EventArgs e)
+        {
+            output.Add(new Product() 
+            { price = 1.05, type = "Produce", item = "Apple - 1.05" });
+            changeText();
+        }
+
+        private void Orange_Click(object sender, EventArgs e)
+        {
+            output.Add(new Product()
+            { price = 0.80, type = "Produce", item = "Orange - 0.80" });
+            changeText();
+        }
+
+        private void Banana_Click(object sender, EventArgs e)
+        {
+            output.Add(new Product()
+            { price = 0.75, type = "Produce", item = "Banana - 0.75" });
+            changeText();
+        }
+
+        private void WindshieldFluid_Click(object sender, EventArgs e)
+        {
+            output.Add(new Product()
+            { price = 15.50, type = "Vehicle", item = "Windshield Fluid - 15.50" });
+            changeText();
+        }
+
+        private void Lube_Click(object sender, EventArgs e)
+        {
+            output.Add(new Product()
+            { price = 12.28, type = "Vehicle", item = "Lube - 12.28" });
+            changeText();
+        }
+
+        private void Pump_Click(object sender, EventArgs e)
+        {
+            output.Add(new Product()
+            { price = 50.99, type = "Vehicle", item = "Pump - 50.99" });
+            changeText();
+        }
+
+        private void Keyboard_Click(object sender, EventArgs e)
+        {
+            output.Add(new Product()
+            { price = 20.00, type = "Electronics", item = "Keyboard - 20.00" });
+            changeText();
+        }
+
+        private void Mouse_Click(object sender, EventArgs e)
+        {
+            output.Add(new Product()
+            { price = 15.00, type = "Electronics", item = "Mouse - 15.00" });
+            changeText();
+        }
+
+        private void Mousepad_Click(object sender, EventArgs e)
+        {
+            output.Add(new Product()
+            { price = 10.00, type = "Electronics", item = "Mousepad - 10.00" });
+            changeText();
+        }
+
+        private void LipBalm_Click(object sender, EventArgs e)
+        {
+            output.Add(new Product()
+            { price = 2.55, type = "Healthcare", item = "Lip Balm - 2.55" });
+            changeText();
+        }
+
+        private void Ointment_Click(object sender, EventArgs e)
+        {
+            output.Add(new Product()
+            { price = 10.55, type = "Healthcare", item = "Ointment - 10.55" });
+            changeText();
+        }
+
+        private void Lotion_Click(object sender, EventArgs e)
+        {
+            output.Add(new Product()
+            { price = 5.55, type = "Healthcare", item = "Lotion - 5.55" });
+            changeText();
+        }
+        //End of product buttons
+
+        //4 - A cancel or void button which,
+        //when pressed, clears and restarts the sale
+        private void Void_Click(object sender, EventArgs e)
+        {  
+            output.Clear();
+            subTotal = 0.0;
+            Tax = 0.0;
+            Total = 0.0;
+            this.Transaction.Text = "";
+            this.Sales.Text = "";
+        }
+
+        //Todo
+        //5 - A save or export button which, when pressed,
+        //saves or exports the current transaction in a JSON, XML, or text format.
+        //Exports to WizTecBasicSalesCalc\bin\Build\netcoreapp3.1\"ExportedText"
+        private void Export_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        //3 - When a product is selected, it is added to the current transaction.
+        //The transaction must support up to 10 different products,
+        //listing product and price, and calculating subtotal, VAT, and total
+
+        //Change the transaction text box
+        private void changeText()
+        {
+            subTotal = 0.0;
+            Tax = 0.0;
+            Total = 0.0;
+            var listString = new StringBuilder();
+            foreach (var p in output)
+            {
+                listString.Append(p.item);
+                listString.Append(Environment.NewLine);
+                if (p.type == "Healthcare")
+                {
+                    subTotal += p.price;
+                    subTotal = Math.Round(subTotal, 2, MidpointRounding.ToZero);
+                }
+                else
+                {
+                    subTotal += p.price;
+                    Tax += (p.price * VAT);
+                    subTotal = Math.Round(subTotal, 2, MidpointRounding.ToZero);
+                    Tax = Math.Round(Tax, 2, MidpointRounding.ToZero);
+                }
+            }
+            Total += subTotal + Tax;
+            Total = Math.Round(Total, 2, MidpointRounding.ToZero);
+            Transaction.Text = listString.ToString();
+            changeSalesText();
+        }
+
+        //Change the sales text box
+        private void changeSalesText()
+        {
+            var salesString = new StringBuilder();
+            List<string> salesText = new List<string>();
+            salesText.Add("Subtotal: ");
+            salesText.Add("Tax: ");
+            salesText.Add("Total: ");
+            for(int i = 0; i < salesText.Count; i++)
+            {
+                if (i == 0) {
+                    salesString.Append(salesText[i] + subTotal);
+                    salesString.Append(Environment.NewLine);
+                }
+                if (i == 1)
+                {
+                    salesString.Append(salesText[i] + Tax);
+                    salesString.Append(Environment.NewLine);
+                }
+                if (i == 2)
+                {
+                    salesString.Append(salesText[i] + Total);
+                    salesString.Append(Environment.NewLine);
+                }
+            }
+            Sales.Text = salesString.ToString();
+        }
+        private void Transaction_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Sales_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
